e370889e3f513c032a8394e19b4a03799ffc6e9a
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-03-06 17:24:09 -0600

    Properly title window

--- graphics.py
+++ graphics.py
@@ -3,7 +3,7 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height) -> None:
         self.__root = Tk()
-        self.__root .title = "maze_solver"
+        self.__root.title("maze_solver")
         self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
