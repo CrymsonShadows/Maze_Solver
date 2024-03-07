74f6918a49fd54b94efefa5a8b575948e6ce59e3
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-03-07 14:21:38 -0600

    Add the ability to add a line to be drawn

--- graphics.py
+++ graphics.py
@@ -1,5 +1,19 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self, x=0, y=0) -> None:
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, point_1: Point, point_2: Point) -> None:
+        self.point_1 = point_1
+        self.point_2 = point_2
+    
+    def draw(self, canvas: Canvas, fill_color="black"):
+        canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
+
 class Window:
     def __init__(self, width, height) -> None:
         self.__root = Tk()
@@ -18,5 +32,8 @@ class Window:
         while(self.__running):
             self.redraw()
     
+    def draw_line(self, line: Line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
     def close(self):
         self.__running = False
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(20, 20), Point(20, 360))
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
