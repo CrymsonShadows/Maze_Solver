be1a73f07be175848550d87f9dabfbb36d7a0d74
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-03-11 18:20:56 -0500

    Change the __ to _ for private members in the cell class

--- cell.py
+++ cell.py
@@ -7,24 +7,24 @@ class Cell:
         self.has_top_wall = top
         self.has_bottom_wall = bottom
         self.fill_color = fill_color
-        self.__x1 = x1
-        self.__y1 = y1
-        self.__x2 = x2
-        self.__y2 = y2
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
         self.__win = win
 
     def draw(self):
         if self.has_left_wall:
-            line = Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2))
+            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
             self.__win.draw_line(line, self.fill_color)
         if self.has_right_wall:
-            line = Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2))
+            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
             self.__win.draw_line(line, self.fill_color)
         if self.has_top_wall:
-            line = Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1))
+            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
             self.__win.draw_line(line, self.fill_color)
         if self.has_bottom_wall:
-            line = Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2))
+            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self.__win.draw_line(line, self.fill_color)
     
     def draw_move(self, to_cell, undo=False):
@@ -33,9 +33,9 @@ class Cell:
             fill_color = "gray"
         else:
             fill_color = "red"
-        self_x_mid = (self.__x2 - self.__x1) // 2 + self.__x1
-        self_y_mid = (self.__y2 - self.__y1) // 2 + self.__y1
-        to_x_mid = (to_cell.__x2 - to_cell.__x1) // 2 + to_cell.__x1
-        to_y_mid = (to_cell.__y2 - to_cell.__y1) // 2 + to_cell.__y1
+        self_x_mid = (self._x2 - self._x1) // 2 + self._x1
+        self_y_mid = (self._y2 - self._y1) // 2 + self._y1
+        to_x_mid = (to_cell._x2 - to_cell._x1) // 2 + to_cell._x1
+        to_y_mid = (to_cell._y2 - to_cell._y1) // 2 + to_cell._y1
         line = Line(Point(self_x_mid, self_y_mid), Point(to_x_mid, to_y_mid))
         self.__win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def main():
     cell2.draw()
     cell3 = Cell(win, 150, 50, 200, 100)
     cell3.draw()
+    cell4 = Cell(win, 150, 0, 200, 50)
+    cell4.draw()
     cell1.draw_move(cell2)
     cell2.draw_move(cell3, True)
+    cell3.draw_move(cell4)
     win.wait_for_close()
 
 main()
